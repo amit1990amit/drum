31c760171536d2be7a3bfba4882604a3cde25205
amit1990amit <44804904+amit1990amit@users.noreply.github.com>
2019-01-27 20:45:50 +0200

    first drum

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var rendomDiceImage1 = "images/dice" + randomNumber1 + ".png";
+document.querySelector("img.dice1").setAttribute("src",rendomDiceImage1);
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var rendomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src",rendomDiceImage2);
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "Player1 won";
+}
+else if(randomNumber1<randomNumber2){
+  document.querySelector("h1").innerHTML = "Player2 won";
+}
+else{
+  document.querySelector("h1").innerHTML = "Tie";
+}
